e2d0b40e0d3444c14a4d6f3c65d6f260cf049656
Artur <arturumbelinov@gmail.com>
2025-08-14 15:50:49 -0300

    Tudo funcionando backup

--- app/Models/Prices.php
+++ app/Models/Prices.php
@@ -2,13 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Prices extends Model
 {
+    use HasUuids;
      protected $fillable = [
         'name',
         'value_buy',
         'value_sell',
     ];
+
+            protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+
 }
