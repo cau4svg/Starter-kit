b42e4ad48791057f84b7190e973ec7f84f44833b
Artur <141283176+Artursvg@users.noreply.github.com>
2025-08-21 17:54:22 -0300

    Generate env file automatically

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+# Gera arquivo .env caso não exista
+if [ ! -f .env ]; then
+  cp .env.example .env
+  php artisan key:generate
+fi
+
 # Espera DB e roda migrate + seed
 until php artisan migrate --force; do
   echo "Waiting for database to be ready..."
