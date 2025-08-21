3f76beadfd1a1c9ab83cd932e8ac1d1a14f63664
Artur <141283176+Artursvg@users.noreply.github.com>
2025-08-21 13:30:37 -0300

    Restrict prices CRUD to admins

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
          $middleware->append(\App\Http\Middleware\ForceJsonResponse::class);
+         $middleware->alias([
+             'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
+         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
