bc70d2d6cb9a8795d1fce7a8d4cc0330efa50db1
Artur <arturumbelinov@gmail.com>
2025-08-21 14:08:09 -0300

    Delete admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,9 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
          $middleware->append(\App\Http\Middleware\ForceJsonResponse::class);
-         $middleware->alias([
-             'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
-         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
