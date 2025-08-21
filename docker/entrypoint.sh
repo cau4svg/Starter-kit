5898957faa142fed54fd7e472b5524d1d33aca3a
Artur <arturumbelinov@gmail.com>
2025-08-21 18:04:24 -0300

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,6 +7,11 @@ if [ ! -f .env ]; then
   php artisan key:generate
 fi
 
+if [ ! -f vendor/autoload.php ]; then
+  composer install --no-interaction --prefer-dist --optimize-autoloader
+fi
+
+
 # Espera DB e roda migrate + seed
 until php artisan migrate --force; do
   echo "Waiting for database to be ready..."
